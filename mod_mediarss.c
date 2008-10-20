2b1f36922c3e6a5c2c1179a8d12b29ad4340677b
Stefan Arentz <stefan@arentz.ca>
2008-10-20 10:43:17 -0400

    Code cleanup. Changed to MD5 hash for GUID. Fixed apr_open_dir() memory leak.

--- mod_mediarss.c
+++ mod_mediarss.c
@@ -17,33 +17,15 @@
  * limitations under the License.
  */
 
+#include <apr_strings.h>
+
 #include <httpd.h>
 #include <http_core.h>
 #include <http_log.h>
 #include <http_protocol.h>
 #include <http_request.h>
 #include <http_config.h>
-
-// TODO Copied this from mod_rewrite .. isn't there a simple shortcut for this in apr?
-
-static char *lookup_header(request_rec *r, const char *name)
-{
-   const apr_array_header_t *hdrs_arr;
-   const apr_table_entry_t *hdrs;
-   int i;
-
-   hdrs_arr = apr_table_elts(r->headers_in);
-   hdrs = (const apr_table_entry_t *)hdrs_arr->elts;
-   for (i = 0; i < hdrs_arr->nelts; ++i) {
-      if (hdrs[i].key == NULL) {
-         continue;
-      }
-      if (strcasecmp(hdrs[i].key, name) == 0) {
-         return hdrs[i].val;
-      }
-   }
-   return NULL;
-}
+#include <util_md5.h>
 
 static int mediarss_index_directory(request_rec* r)
 {
@@ -57,7 +39,7 @@ static int mediarss_index_directory(request_rec* r)
    }
 
    /* Content header */
-
+   
    char* url;
    url = ap_construct_url(r->pool, r->uri, r);
 
@@ -89,16 +71,18 @@ static int mediarss_index_directory(request_rec* r)
          rr = ap_sub_req_lookup_dirent(&dirent, r, AP_SUBREQ_NO_ARGS, NULL);
          if (rr != NULL)
          {
-            //if (rr->finfo.filetype == APR_REG && rr->status == OK)
+            if (rr->finfo.filetype == APR_REG && rr->status == HTTP_OK)
             {
                char size[16];
                snprintf(size, sizeof(size), "%d", dirent.size);
                
                char date[APR_RFC822_DATE_LEN];
                apr_rfc822_date(date, dirent.mtime);
+               
+               char* guid = ap_md5(r->pool, (unsigned char*) apr_pstrcat(r->pool, url, dirent.name, NULL));
                            
                ap_rputs("  <item>\n", r);
-               ap_rvputs(r, "    <guid>", url, dirent.name, "</guid>\n", NULL);
+               ap_rvputs(r, "    <guid>", guid, "</guid>\n", NULL);
                ap_rvputs(r, "    <title>", dirent.name, "</title>\n", NULL);
                ap_rvputs(r, "    <pubDate>", date, "</pubDate>\n", NULL);
                ap_rvputs(r, "    <enclosure url=\"", url, dirent.name, "\" length=\"", size, "\"\n", NULL);
@@ -118,6 +102,8 @@ static int mediarss_index_directory(request_rec* r)
 
    ap_rputs("</channel>\n", r);
    ap_rputs("</rss>\n", r);
+
+   apr_dir_close(dir);
    
    return OK;
 }
@@ -135,15 +121,11 @@ static int mediarss_handler(request_rec* r)
       return DECLINED;
    }
    
-   if (r->method_number != M_GET) {
-      return DECLINED;
-   }
-
-   // TODO This is lame and should be done in a different way
-
-   const char* user_agent = lookup_header(r, "User-Agent");
+   // TODO This is lame and should be done in a totally different way
+   
+   const char* user_agent = apr_table_get(r->headers_in, "User-Agent");
    if (user_agent != NULL && strstr(user_agent, "iTunes") == NULL) {
-      const char* accept = lookup_header(r, "Accept");
+      const char* accept = apr_table_get(r->headers_in, "Accept");
       if (accept == NULL || strcmp(accept, "application/rss+xml") != 0) {
          return DECLINED;
       }
